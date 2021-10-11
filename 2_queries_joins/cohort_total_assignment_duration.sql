68b7e918d812e55c42e2e80254bcd3f7882e3777
Thainnt <thai164@gmail.com>
2021-10-11 21:56:19 +0000

    Add BootcampX Queries II - JOIN assignment

---
+++ 2_queries_joins/cohort_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT SUM(duration)
+AS total_duration
+FROM students 
+JOIN assignment_submissions ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12'
+GROUP BY cohorts.id;

b8f8dee5b4ec1b4a7d7a7f69b0647d95fb8591b4
Thainnt <thai164@gmail.com>
2021-10-12 21:54:19 +0000

    Add BootcampX Queries 4 assignment

---
+++ 4_queries/average_cohort_assistance_time.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS name, SUM(completed_at - started_at) / COUNT(assistance_requests.id) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
